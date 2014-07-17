26739660c976c71829c19b2ab27c0dcaf3c08586
satya_prakash <satya@hostnsoft.com>
2014-07-17 16:27:54 +0530

    Some more

--- index.php
+++ index.php
@@ -146,23 +146,11 @@ foreach ( $files as $file_id=>$file ) {
   
     <dd>
         <div class="mutliSelect">
-            <ul>
-                <li>
-                    <input type="checkbox" value="Apple" />Apple</li>
-                <li>
-                    <input type="checkbox" value="Blackberry" />Blackberry</li>
-                <li>
-                    <input type="checkbox" value="HTC" />HTC</li>
-                <li>
-                    <input type="checkbox" value="Sony Ericson" />Sony Ericson</li>
-                <li>
-                    <input type="checkbox" value="Motorola" />Motorola</li>
-                <li>
-                    <input type="checkbox" value="Nokia" />Nokia</li>
+            <ul id = "chooseColumn" class = "classChooseColumn">
+                
             </ul>
         </div>
     </dd>
-  <button>Filter</button>
 </dl>
                     
                     
